65daa133fbc2cb980b7d2190c4276a9ee2489bec
codewhite7777 <codewhite7777@gmail.com>
2026-04-01 01:43:13 +0900

    main.py 루프문 기본 구조 설정

---
+++ main.py
@@ -0,0 +1,24 @@
+while True:
+    print('========================')
+    print('나만의 퀴즈 게임')
+    print('========================')
+    print('1. 퀴즈 풀기')
+    print('2. 퀴즈 추가')
+    print('3. 퀴즈 목록')
+    print('4. 점수 확인')
+    print('5. 종료')
+    print('========================')
+    opt = int(input('선택'))
+    
+    if opt == 1:
+        print('퀴즈를 시작합니다.')
+    elif opt == 2:
+        print('퀴즈를 추가합니다')
+    elif opt == 3:
+        print('등록된 퀴즈 목록 (총 n개')
+    elif opt == 4:
+        print('최고 점수 : n점 (k문제 중 p개 정답)')
+    elif opt == 5:
+        print('종료')
+    else:
+        print('잘못된 입력입니다.')
